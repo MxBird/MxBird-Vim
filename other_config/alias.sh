0f9ae371fdad8431c4851f18d189455f6f7f3925
Ray <ray.artisan@gmail.com>
2021-10-21 09:28:11 +0800

    update config

--- other_config/alias.sh
+++ other_config/alias.sh
@@ -5,12 +5,12 @@ alias start_memcached="/usr/local/opt/memcached/bin/memcached -d"
 #Zeus
 alias start_zeus="zeus start"
 
-# Nginx
+#Nginx
 alias start_nginx="sudo nginx"
 alias restart_nginx="sudo nginx -s reload"
 alias stop_nginx="sudo nginx -s stop"
 
-# Mysql
+#Mysql
 alias start_mysql="mysql.server start"
 #alias start_mysql="/usr/local/opt/mysql55/bin/mysql.server start"
 ##alias stop_mysql="/usr/local/opt/mysql55/bin/mysql.server stop"
@@ -19,7 +19,7 @@ alias stop_mysql="mysql.server stop"
 #zookeeper
 alias start_ZK="zkServer start"
 
-# Mongodb
+#Mongodb
 alias start_mongodb="mongod --fork --config /usr/local/etc/mongod.conf"
 alias stop_mongodb="mongo admin --eval \"db.shutdownServer()\""
 alias rm_mongodblock="rm /usr/local/var/mongodb/mongod.lock"
@@ -32,24 +32,25 @@ if [ -f $REDIS_PID ]; then
 	alias stop_redis="kill `cat $REDIS_PID`"
 fi
 
-# Git
+#Git
 alias gc_master="git branch --merged master | grep -v \"\* master\" | grep -v \"develop\"| xargs -n 1 git branch -d"
 alias gc_develop="git branch --merged develop | grep -v \" master\" | grep -v \"develop\" | xargs -n 1 git branch -d"
 alias gc_head="git branch --merged | grep -v \"\*\" | grep -v \"develop\" | xargs -n 1 git branch -d"
 alias gc_ci="git branch -r | grep ci_ | sed 's/origin\///g' | xargs -n 1 git push origin --delete"
 
-# DNS
+#DNS
 alias ehost="sudo vi /etc/hosts"
 alias clean_dns_cache="sudo killall -HUP mDNSResponder"
 
-# System command
+#System command
 alias clean_memory='sudo purge'
 alias update_software='sudo softwareupdate -i -a'
 
-# cap deploy
+#cap deploy
 alias deploy_staging="cap staging deploy"
 alias deploy_production="cap production deploy"
 
 #Other
 alias up='uptime'
+alias tree='tree -N'
 alias reload_alias="source ~/.alias.sh"
